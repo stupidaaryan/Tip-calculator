02f1f033317cf16b386b11b6ac0039f1035d21f2
Aryan Sinha <141643484+stupidaaryan@users.noreply.github.com>
2023-08-08 10:49:48 +0530

    main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ tip = int(input("What percentage of tip You would like to give? 12, 10 or 15? \n
 people = int(input("how many people to split the bill?"))
 real_tip = total_bill * (tip/ 100)
 
-splited_bill = ((total_bill + real_tip)/ people)
+splited_bill = "{:.2f}".format((total_bill + real_tip)/ people)
 
 print(f"Each person should pay {splited_bill}")
